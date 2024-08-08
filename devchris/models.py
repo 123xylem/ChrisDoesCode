27ca9e6f0fb5e9eb4e8e4b3c42a638468222fc2e
123Xylem <cucullen111@gmail.com>
2024-08-08 09:30:02 +0100

    add django-filer img on skill model

--- devchris/models.py
+++ devchris/models.py
@@ -2,14 +2,31 @@ import datetime
 from django.db import models
 from datetime import datetime
 from django.utils.safestring import mark_safe
+from filer.fields.image import FilerImageField
+
 
 def get_upload_path(instance, filename):
     return 'static/images/{}/{}'.format(datetime.now().strftime('%Y/%m'), filename)
 
 class ImageMixinClass():
   def image_preview(self):
-    if self.img and hasattr(self.img, 'url'):
-      return mark_safe('<img src="%s" width="150" height="150" />' % (self.img.url))
+    # print(self.__dict__.items())
+      html= ""
+      for field in self._meta.fields:
+          field_value = getattr(self, field.name)
+          if isinstance(field, models.ImageField):
+              if field_value:
+                  url = field_value.url if hasattr(field_value, 'url') else field_value
+                  html += '<img src="%s" width="150" height="150" />' % url
+          # Preview django-filer img?
+          elif isinstance(field, FilerImageField):
+              if field_value and hasattr(field_value, 'url'):
+                  html += '<img src="%s" width="150" height="150" />' % field_value.url
+
+      return mark_safe(html)
+    # if self.img and hasattr(self.img, 'url'):
+
+    #   return mark_safe('<img src="%s" width="150" height="150" />' % (self.img.url))
   image_preview.allow_tags = True
   image_preview.short_description = 'Preview'
 
@@ -22,7 +39,10 @@ class titleMixinClass():
 # Text UL Image - for HomePage skills
 class Skill(ImageMixinClass, titleMixinClass, models.Model):
     title = models.CharField(max_length=300, default="title")
-    img = models.ImageField(upload_to=get_upload_path)
+    # img = models.ImageField(upload_to=get_upload_path)
+    img = FilerImageField(related_name="skill_img", null=True, blank=True, on_delete=models.CASCADE)
+    # test_img = FilerImageField(related_name="book_covers", null=True, blank=True, on_delete=models.CASCADE)
+
     class Meta:
       ordering = ["title"]
 
