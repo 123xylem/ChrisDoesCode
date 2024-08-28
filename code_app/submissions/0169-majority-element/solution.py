1a5848a8d5bba73daa49fa4e51e2d12fa7aa74a0
123Xylem <cucullen111@gmail.com>
2024-08-28 11:34:08 +0100

    remove submissions from app

--- code_app/submissions/0169-majority-element/solution.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        count = defaultdict(list)
-        half = len(nums) / 2
-        for num in nums:
-            count[num] = count.get(num, 0) + 1
-            if count[num] >= half:
-                return num
--- code_app/submissions/2058-concatenation-of-array/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def getConcatenation(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        ans = nums + nums
-        print(nums, ans)
-        return  ans
