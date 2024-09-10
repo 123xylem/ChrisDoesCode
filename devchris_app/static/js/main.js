ea1d00ed6318539066bc745f48d73e5b39803d0a
123Xylem <cucullen111@gmail.com>
2024-09-10 15:50:38 +0100

    spam prevention on form

--- devchris_app/static/js/main.js
+++ devchris_app/static/js/main.js
@@ -1,16 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   burger = document.querySelector(".hamburger");
   navClose = document.querySelector(".close-nav");
-  logoDiv = document.getElementById("site-name");
   if (burger) {
     burger.addEventListener("click", toggleMenu);
   }
   if (navClose) {
     navClose.addEventListener("click", toggleMenu);
-  }
-
-  if (logoDiv) {
-    logoDiv.addEventListener("click", goHome);
   }
 
   function toggleMenu() {
@@ -40,9 +35,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const contactForm = document.getElementById("my-contact-form");
   const fMessage = document.getElementById("form-message");
-
   contactForm.addEventListener("submit", function (e) {
     e.preventDefault();
+    if (!customFormTextareaValidation(".form-message textarea")) {
+      return "Spam Detected";
+    }
+
     var formData = new FormData(this);
     fetch(this.action, {
       method: "POST",
@@ -74,5 +72,59 @@ document.addEventListener("DOMContentLoaded", function () {
       }
       document.getElementById("my-contact-form").reset();
     }, 1200);
+  }
+
+  function customFormTextareaValidation(selector) {
+    // List of blocked words
+    let blockedWords = [
+      " seo ",
+      "marketing",
+      "plugin",
+      "website",
+      "backlink",
+      "link building",
+      "emails",
+      " market",
+      " ai ",
+      "million",
+      "billion",
+      " users",
+      " volume",
+      " spam",
+      "business",
+      "healthy",
+      "drug",
+    ];
+    let blocked = false;
+
+    // Get the textarea field value and convert to lowercase
+    let fieldSubmit = document.querySelector(selector).value.toLowerCase();
+
+    // Regular expressions for Russian, Chinese, and Cyrillic characters
+    let russianRegex = /[\u0400-\u04FF]/;
+    let chineseRegex = /[\u4E00-\u9FFF]/;
+    let cyrillicRegex = /[\u0400-\u04FF]/;
+
+    blockedWords.forEach(function (word) {
+      if (fieldSubmit.includes(word)) {
+        blocked = true;
+      }
+    });
+
+    if (
+      russianRegex.test(fieldSubmit) ||
+      chineseRegex.test(fieldSubmit) ||
+      cyrillicRegex.test(fieldSubmit)
+    ) {
+      blocked = true;
+    }
+
+    // If blocked content is found, show an alert message or handle error as needed
+    if (blocked) {
+      alert("Your message contains prohibited content.");
+      return false;
+    }
+
+    return true;
   }
 });
